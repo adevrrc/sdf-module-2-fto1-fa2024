9058a6941aa7c9fdbdff63e0b90aabf9e481516c
Damien Altenburg <daltenburg@rrc.ca>
2024-09-09 17:04:08 -0500

    Add script that demonstrates data types and built-in functions.

---
+++ module_2_data_types.py
@@ -0,0 +1,22 @@
+"""
+Description: This script demonstrates data types in python.
+Author: Damien Altenburg
+Date: 9/9/2024
+Usage: python module_2_data_types.py
+"""
+# Invoking Function Syntax
+# function_name(argument, argument, etc)
+print('Hello World', "Damien", "is super cool.")
+
+#print(*objects, sep=' ', end='\n', file=None, flush=False)
+print("Hello World", "Damien", "is super cool.", sep=',')
+
+student_full_name = "Damien Altenburg"
+
+age = 26
+
+current_salary = 45238.89
+
+is_employed = True
+
+print(student_full_name)
