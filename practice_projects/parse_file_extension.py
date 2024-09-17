61397475a1facd43572fa3dc7aef51551a25e416
Damien Altenburg <daltenburg@rrc.ca>
2024-09-17 15:26:34 -0500

    Add script; module 2 practice problem 12.

---
+++ practice_projects/parse_file_extension.py
@@ -0,0 +1,14 @@
+"""
+Description: The script parses a file extension from a file path.
+Author: Damien Altenburg
+Date: 2024-9-17
+Usage: python parse_file_extensions.py
+"""
+filename = input("Enter a filename: ")
+
+dot_index = filename.index('.')
+
+file_extension_start_index = dot_index + 1
+file_extension = filename[file_extension_start_index:]
+
+print(f"File extension: {file_extension}")
